1a756c3e787e989cb2f2c1f06484b02dafe9201b
haryo wicaksono <har.wicak@gmail.com>
2024-08-24 14:33:35 +0700

    chore: load_dotenv

--- config/config.py
+++ config/config.py
@@ -1,9 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 PORT = int(os.environ.get("PORT", 3000))
 
 LLM_API_BASE_URL = os.getenv("LLM_API_BASE_URL")
 LLM_API_KEY = os.getenv("LLM_API_KEY")
 LLM_CHAT_MODEL = os.getenv("LLM_CHAT_MODEL")
-LLM_STREAMING = os.getenv("LLM_STREAMING", "yes").lower() != "no"
-
